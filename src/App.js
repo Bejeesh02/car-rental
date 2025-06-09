597216d592a1baf8c7c89803718dced90fd8cb6a
Bejeesh02 <bejeeshb2002@gmail.com>
2025-06-09 10:52:46 +0530

    Fix deployment script and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import {HashRouter as Router,Route,Routes,Navigate} from 'react-router-dom';
-import Navbar from './navbar.js';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login.js';
 import Home from './pages/home.js';
 import Footer from './footer.js';
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        
           <div className='container'>
             <Routes>
               <Route path="/" element={<Login setUser={setUser} />}/>
