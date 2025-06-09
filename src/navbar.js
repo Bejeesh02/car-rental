72729a971fbcd9647084122b0b6e56a72e90954c
Bejeesh02 <bejeeshb2002@gmail.com>
2025-06-09 10:57:12 +0530

    Fix deployment script and routing

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+    return(
+      <nav className="navbar">
+        <ul className="nav-link">
+           
+        </ul>
+      </nav>
+    );
+}
+
+export default Navbar;
